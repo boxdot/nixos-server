e25c97556ddea66c561085161abdae3f357a3307
boxdot <d@zerovolt.org>
2015-11-06 20:00:25 +0100

    Initial configuration.

---
+++ configuration.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/vda";
+
+  networking.hostName = "nixos";
+
+  time.timeZone = "Europe/Berlin";
+
+
+  # Programs
+
+  environment.systemPackages = with pkgs; [
+     wget
+     vim
+     git
+  ];
+
+  programs.zsh.enable = true;
+
+
+  # Services
+
+  services = {
+
+    openssh = {
+      enable = true;
+      permitRootLogin = "no";
+      passwordAuthentication = false;
+    };
+
+    nginx = {
+      enable = true;
+    };
+
+  };
+
+
+  # Users
+
+  users.extraUsers.nixos = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+  };
+
+  users.extraUsers.dima = {
+    isNormalUser = true;
+    uid = 1001;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDZ6cQd+3L2dbeB6jEl3lUeiYsxYuN7FO4AQMCz/SpEdBXjKAQlz7L5jUo+4EGux4ziF7pDlVPZpV6WSoW5kmJc= dimw@snow" ];
+    shell = "/run/current-system/sw/bin/zsh";
+  };
+
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+
+  system.stateVersion = "15.09";
+
+}
